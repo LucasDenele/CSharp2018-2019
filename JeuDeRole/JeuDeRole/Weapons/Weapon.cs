e4c5eb93861f1aa7e14d30c760eda1c88970cc94
LucasDenele <lucas.denele@isen.yncrea.fr>
2019-01-07 16:40:28 +0100

    Avancement tp (demonstration git)

---
+++ JeuDeRole/JeuDeRole/Weapons/Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuDeRole
+{
+    public abstract class Weapon
+    {
+        #region Variables
+        public string Name { get; private set;}
+
+        public int Damages { get; private set; }
+        #endregion
+
+        #region Constructor
+        public Weapon(String NewName, int NewDamages)
+        {
+            this.Name = NewName;
+            this.Damages = NewDamages;
+        }
+        #endregion
+    }
+}
